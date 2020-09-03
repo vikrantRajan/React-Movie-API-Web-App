3b36078b4c2e7c1322706ab2b34a7e922cdf52d8
Vikrant Rajan <vikrant_rajan@thecdm.ca>
2020-09-03 01:21:47 -0700

    React Movie App Base

---
+++ build/precache-manifest.14362de96acb4186696f827b3c677458.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e96fdebf1dba7aa8445a69b1e0ebf06",
+    "url": "/ReactMovieApp/index.html"
+  },
+  {
+    "revision": "1b1a66892f867984ea3a",
+    "url": "/ReactMovieApp/static/css/main.83774afa.chunk.css"
+  },
+  {
+    "revision": "6426f5b1d7713e615f48",
+    "url": "/ReactMovieApp/static/js/2.8412a9e2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ReactMovieApp/static/js/2.8412a9e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b1a66892f867984ea3a",
+    "url": "/ReactMovieApp/static/js/main.0c87fb87.chunk.js"
+  },
+  {
+    "revision": "d8d203c4bd43bdc09998",
+    "url": "/ReactMovieApp/static/js/runtime-main.7080fd4d.js"
+  }
+]);
---
+++ src/components/elements/NotFound/NotFound.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NotFound = () => {
+    return (
+        <div>
+            <h1>Sorry, This information does not exist</h1>
+        </div>
+    )
+}
+
+export default NotFound;
